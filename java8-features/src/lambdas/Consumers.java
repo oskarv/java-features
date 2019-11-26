d2d4ab767fdd778264eb5a19dead484511901615
Oskar Vatic <oskarsmit@hotmail.com>
2019-11-26 23:23:36 +0100

    chain consumers example

--- java8-features/src/lambdas/Consumers.java
+++ java8-features/src/lambdas/Consumers.java
@@ -1,7 +1,9 @@
 package lambdas;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Consumers {
 
@@ -9,11 +11,28 @@ public class Consumers {
         List<String> names = Arrays.asList("Oskar", "Robert Martin", "Martin Fowler");
 
         // using standard Consumer Lambda
+        System.out.println("using standard Consumer Lambda");
         names.forEach(name -> System.out.println(name));
 
         System.out.println("========================**========================");
 
         // using method reference (shorter, more readable syntax)
+        System.out.println("using method reference (shorter, more readable syntax)");
         names.forEach(System.out::println);
+
+        System.out.println("========================**========================");
+
+        List<String> resultNames = new ArrayList<>();
+
+        Consumer<String> printConsumer = System.out::println;
+        Consumer<String> insertConsumer = resultNames::add; // or: insertConsumer = name -> resultNames.add(name);
+
+        System.out.println("chain consumers, one prints other inserts into another collection");
+        names.forEach(printConsumer.andThen(insertConsumer));
+
+        System.out.println("========================**========================");
+        System.out.println("Result array");
+
+        resultNames.forEach(System.out::println);
     }
 }
