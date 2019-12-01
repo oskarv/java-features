a95d6ca34ad3c1a265abb57d1786aeeab097f448
Oskar Vatic <oskarsmit@hotmail.com>
2019-12-01 10:46:10 +0100

    Fix import star code style

--- java8-features/src/streams/ReduceStreamExamples.java
+++ java8-features/src/streams/ReduceStreamExamples.java
@@ -1,6 +1,9 @@
 package streams;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 public class ReduceStreamExamples {
 
