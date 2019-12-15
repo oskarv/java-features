115ef4f3082c432be468a84d5f7f9a153a418e87
Oskar Vatic <oskarsmit@hotmail.com>
2019-12-15 23:53:59 +0100

    method scoped generics

--- java8-features/src/generics/inheritance/ComparatorsMain.java
+++ java8-features/src/generics/inheritance/ComparatorsMain.java
@@ -3,6 +3,7 @@ package generics.inheritance;
 import generics.collections.dto.Person;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class ComparatorsMain {
@@ -21,5 +22,25 @@ public class ComparatorsMain {
         people.sort(new ReverseComparator<Person>(new AgeComparator()));
         System.out.println(people);
         System.out.println("00----------------------------------------------------------00");
+
+        Person min = min(people, new AgeComparator());
+        System.out.println("Min is: " + min);
+        System.out.println("00----------------------------------------------------------00");
+    }
+
+    public static <T> T min (List<T> values, Comparator<T> comparator) {
+        if (values.isEmpty()) {
+            throw new IllegalArgumentException("List of values is empty");
+        }
+
+        T min = values.get(0);
+
+        for(T value : values) {
+            if (comparator.compare(value, min) < 0) {
+                min = value;
+            }
+        }
+
+        return min;
     }
 }
