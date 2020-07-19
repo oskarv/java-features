63e5eb0c21dbacc214050a76d86bc5256e86533c
Oskar Vatic <oskarsmit@hotmail.com>
2020-07-20 00:05:42 +0200

    Fix few typos

--- java8-features/src/concurrency/RunnablePattern.java
+++ java8-features/src/concurrency/RunnablePattern.java
@@ -15,22 +15,22 @@ class RunnablePattern {
         Thread thread = new Thread(task);
         thread.start();
 
-        // Does the some as lines above
+        // Does the same as lines above
         new Thread(() -> System.out.println("What does this do? -- " + Thread.currentThread().getName())).start();
 
-        // Executor doesn't create new thread, it always reuses the same one
+        // Executor doesn't create a new thread, it always reuses the same one
         ExecutorService executor = Executors.newSingleThreadExecutor();
         // tasks are executed in the order of submission
         executor.execute(task);
         executor.execute(() -> System.out.println("new task -- " + Thread.currentThread().getName()));
 
-        // Runnable can't return results or throw errors, Callable is used when we need those two
+        // Runnable can't return any results or throw errors, Callable is used when we need those two
         Callable<List<String>> callable = () -> Arrays.asList("**", "****", "*", "***");
         Future<List<String>> futureResult = executor.submit(callable);
 
         try {
-            // get is a blocking call until results are available!
-            // we can pass time to get() method so we don't block the world forever
+            // get is a blocking call, it blocks until results are available!
+            // we can pass time to get() method so we don't block the world forever in case something stuck
             System.out.println(futureResult.get() + " -- " + Thread.currentThread().getName());
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
