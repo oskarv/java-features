c0e54f792e8ef38b08186c7ed1ca2ff2ef39ebcc
Oskar Vatic <oskarsmit@hotmail.com>
2020-07-20 00:00:04 +0200

    Java concurrency, runnable patern / executor service

---
+++ java8-features/src/concurrency/RunnablePattern.java
@@ -0,0 +1,41 @@
+package concurrency;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class RunnablePattern {
+
+    public static void main (String [] argv) {
+        Runnable task = () -> System.out.println("thread code running ...  -- " + Thread.currentThread().getName());
+        Thread thread = new Thread(task);
+        thread.start();
+
+        // Does the some as lines above
+        new Thread(() -> System.out.println("What does this do? -- " + Thread.currentThread().getName())).start();
+
+        // Executor doesn't create new thread, it always reuses the same one
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        // tasks are executed in the order of submission
+        executor.execute(task);
+        executor.execute(() -> System.out.println("new task -- " + Thread.currentThread().getName()));
+
+        // Runnable can't return results or throw errors, Callable is used when we need those two
+        Callable<List<String>> callable = () -> Arrays.asList("**", "****", "*", "***");
+        Future<List<String>> futureResult = executor.submit(callable);
+
+        try {
+            // get is a blocking call until results are available!
+            // we can pass time to get() method so we don't block the world forever
+            System.out.println(futureResult.get() + " -- " + Thread.currentThread().getName());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
